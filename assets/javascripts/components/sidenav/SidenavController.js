f388767c32d77311b9b102e0e3f9fd6678e39ca5
alvinrindra <alvinrindra@gmail.com>
2018-02-11 18:49:12 +0100

    update sidenav controller

--- assets/javascripts/components/sidenav/SidenavController.js
+++ assets/javascripts/components/sidenav/SidenavController.js
@@ -8,24 +8,68 @@ define([
     angular.module('autolinks.sidenav', []);
     angular.module('autolinks.sidenav')
         // Viewer Controller
-        .controller('SidenavController', ['$scope', '$rootScope', '$timeout', '$mdSidenav', '$log', 'EntityService', 'EndPointService',
-        function ($scope, $rootScope, $timeout, $mdSidenav, $log, EntityService, EndPointService) {
+        .controller('SidenavController', ['$scope', '$rootScope', '$timeout', '$mdSidenav', '$log', 'EntityService', 'EndPointService', '_',
+        function ($scope, $rootScope, $timeout, $mdSidenav, $log, EntityService, EndPointService, _) {
+
+          $scope.label = '';
 
           $scope.init = function() {
-            // $timeout( function(){
+            $timeout( function() {
+              debugger;
               $scope.selectedEntity = EntityService.getRootScopeEntity();
-              console.log($scope);
-            // }, 1000);
+              var entity = $scope.selectedEntity;
+              if (entity._private) {
+                $scope.label = entity._private.data.name;
+              }
+              console.log($scope.selectedEntity);
+              // console.log($scope);
+            }, 1000);
           }
 
           $rootScope.$on('sidenavReinit', function (event, args) {
             $scope.init();
-            console.log('sidenavReinit');
           });
 
           $scope.init();
           // $scope.selectedEntity = EntityService.getRootScopeEntity();
           // console.log(selectedEntity);
+
+          // // add Edges to the edges object, then broadcast the change event
+          $scope.update = function() {
+              const entity = $scope.selectedEntity;
+              const label = $scope.label;
+
+              const before = {
+                "rid": entity.data('rid'),
+                "cid": entity.data('cid'),
+                "metadata": entity.data('metadata') ? entity.data('metadata') : {},
+                "value": entity.data('value') ? entity.data('value') : {}
+              };
+              const after = {
+                "rid": entity.data('rid'),
+                "cid": entity.data('cid'),
+                "metadata": $scope.selectedEntity.data().name ? { label: $scope.selectedEntity.data().name } : {},
+                "value": entity.data('value') ? entity.data('value') : {}
+              };
+              const data = { before: before, after: after};
+              EndPointService.editResource(data);
+
+              // $scope.selectedEntity = EntityService.updateRootScopeEntity($scope.selectedEntity);
+              // // broadcasting the event
+              // // $rootScope.$broadcast('appChanged');
+              // $mdSidenav('right').close();
+          };
+
+          $scope.createCompound = function(){
+              $rootScope.$emit('createCompound');
+              $mdSidenav('right').close();
+          };
+
+          $scope.delete = function(){
+              EntityService.deleteEntity();
+              $mdSidenav('right').close();
+          };
+
           $scope.close = function () {
             // Component lookup should always be available since we are not using `ng-if`
             // $route.reload();
